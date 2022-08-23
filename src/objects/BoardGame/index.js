10291e3b922de2a62585a1636b0ed6892f0d2841
unknown <jvrhodenmachado@hotmail.com>
2022-08-23 17:49:30 -0300

    start

---
+++ src/objects/BoardGame/index.js
@@ -0,0 +1,14 @@
+import './style.css'
+import CardGame from "../../components/CardGame";
+
+function BoardGame(amountCards) {
+    const $htmlContent = CardGame().repeat(amountCards)
+
+    return `
+    <section class="board-game">
+        ${$htmlContent}
+    </section>
+    `;
+}
+
+export default BoardGame;
